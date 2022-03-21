0003f24dc056d0f4ca092196355297409fa11cc4
Matt Eland <matt.eland@gmail.com>
2022-03-20 23:00:52 -0400

    Added model

--- MLNet/MattEland.ML.WhoML/MattEland.ML.TimeAndSpace/Program.cs
+++ MLNet/MattEland.ML.WhoML/MattEland.ML.TimeAndSpace/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.ML;
+
 Console.WriteLine("Hello, Time and Space!");
+
+// Regression - Predict the Rating of a Doctor Who episode
+
+// Context
+MLContext context = new();
+
+// Load
+IDataView data = context.Data.LoadFromTextFile<Episode>(
+        path: "WhoDataSet.csv", 
+        separatorChar: ',', 
+        hasHeader: true, 
+        allowQuoting: true, 
+        trimWhitespace: true
+    );
+
+Console.WriteLine("Allons-y!");
